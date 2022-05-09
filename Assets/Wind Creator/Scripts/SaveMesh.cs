7acdbfed2937522d0b5ffea514d6d804a7f0bdca
EagleOwle <Dvoryninovich@gmail.com>
2022-05-09 19:10:53 +0300

    Smoll Edit

---
+++ Assets/Wind Creator/Scripts/SaveMesh.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SaveMesh : MonoBehaviour
+{
+	public bool optimizeMesh;
+	public MeshFilter meshFilter;
+
+	private void OnEnable()
+	{
+		meshFilter = GetComponent<MeshFilter>();
+	}
+}
