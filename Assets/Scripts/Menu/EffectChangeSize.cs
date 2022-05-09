4dedf20f06faa23f73552c6a66a37bdfaa456262
EagleOwle <Dvoryninovich@gmail.com>
2022-05-09 17:05:02 +0300

    Edit input button

---
+++ Assets/Scripts/Menu/EffectChangeSize.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading.Tasks;
+
+public class EffectChangeSize : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.1f;
+
+    private void OnEnable()
+    {
+        transform.localScale = Vector3.zero;
+        Processe();
+    }
+
+    private async void Processe()
+    {
+        while (transform.localScale.x < 1)
+        {
+            await Task.Yield();
+
+            transform.localScale += Vector3.one * (speed * Time.deltaTime);
+        }
+        
+    }
+}
